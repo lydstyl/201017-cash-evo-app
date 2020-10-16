e6f1b7ac04c7d57ef6236feab8a720069e46f316
Gabriel Brun <lydstyl@gmail.com>
2020-10-16 18:23:59 +0200

    first fetch & response from api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
+  const [response, setResponse] = useState({})
+
+  useEffect(() => {
+    ;(async () => {
+      try {
+        const scheme = 'https'
+        const authority = 'cash-evo-api.herokuapp.com' // or host here
+        const path = '/api/v1/users'
+
+        const response = await fetch(`${scheme}://${authority}${path}`)
+
+        const data = await response.json()
+
+        setResponse(data)
+      } catch (error) {
+        console.log('App -> error', error)
+      }
+    })()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        {response && (
+          <pre style={{ textAlign: 'left' }}>
+            {JSON.stringify(response, null, 4)}
+          </pre>
+        )}
+
+        <img src={logo} className='App-logo' alt='logo' />
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
