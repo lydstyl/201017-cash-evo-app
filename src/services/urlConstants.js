f78813100dcbe84cbe11d34a9a7aacf2e7908f99
Gabriel Brun <lydstyl@gmail.com>
2020-10-17 12:17:33 +0200

    REACT_APP_ENV

--- src/services/urlConstants.js
+++ src/services/urlConstants.js
@@ -1,5 +1,17 @@
+let scheme
+let authority
+const pathBegin = process.env.REACT_APP_URL_PATH_BEGIN
+
+if (process.env.REACT_APP_ENV === 'production') {
+  scheme = process.env.REACT_APP_URL_SCHEME
+  authority = process.env.REACT_APP_URL_AUTHORITY // or host here
+} else {
+  scheme = process.env.REACT_APP_DEV_URL_SCHEME
+  authority = process.env.REACT_APP_DEV_URL_AUTHORITY
+}
+
 export const UrlConstants = {
-  scheme: process.env.REACT_APP_URL_SCHEME,
-  authority: process.env.REACT_APP_URL_AUTHORITY, // or host here
-  pathBegin: process.env.REACT_APP_URL_PATH_BEGIN,
+  scheme,
+  authority,
+  pathBegin,
 }
