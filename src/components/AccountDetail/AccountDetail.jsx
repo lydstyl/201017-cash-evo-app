90947f3081f0d0436af6e3c9faf5e808d0f05e7b
Gabriel Brun <lydstyl@gmail.com>
2020-10-18 17:41:32 +0200

    dayjs & account.moments.map

--- src/components/AccountDetail/AccountDetail.jsx
+++ src/components/AccountDetail/AccountDetail.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
 import { useParams } from 'react-router-dom'
+import dayjs from 'dayjs'
 
 import { AppContext } from '../App/App'
 
@@ -10,9 +11,25 @@ export const AccountDetail = () => {
 
   const account = appContext.appState.accounts.find((a) => a.id == id)
 
+  let moments
+
+  if (account) {
+    moments = account.moments.map((m) => {
+      return {
+        amount: m.amount,
+
+        createdAt: dayjs(m.createdAt, 'MM-DD-YYYY').format(
+          'DD/MM/YYYY - HH:mm:ss'
+        ),
+      }
+    })
+  }
+
   return (
     <div>
-      <pre>{JSON.stringify(account, null, 4)}</pre>
+      {account && <h1>{account.name}</h1>}
+
+      <pre>{JSON.stringify(moments, null, 4)}</pre>
     </div>
   )
 }
